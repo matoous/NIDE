e03370a1b960333f431bd2557cdefa95b1e92d1d
Matouš Dzivjak <matousdzivjak@gmail.com>
2021-08-29 15:21:07 +0200

    feat(telescope): put prompt on top

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,11 +1,15 @@
 local actions = require('telescope.actions')
 require('telescope').setup({
-    defaults = {
-        mappings = {
-            i = {
-                ['<esc>'] = actions.close,
-            },
+  defaults = {
+    layout_config = {
+      prompt_position = "top",
+    },
+    sorting_strategy = "ascending",
+    mappings = {
+        i = {
+            ['<esc>'] = actions.close,
         },
     },
+  },
 })
 
