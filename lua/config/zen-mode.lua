544aad3141a523557b2a7f0388a074187f6acedb
Matouš Dzivjak <matousdzivjak@gmail.com>
2021-09-03 10:28:07 +0200

    fix(plugins): remove config for unused zen-mode

--- lua/config/zen-mode.lua
+++
@@ -1,8 +0,0 @@
--- TODO: remove, unused
-require"zen-mode".setup {
-  window = { backdrop = 1, options = { signcolumn = "no" } },
-  plugins = { tmux = true },
-}
-
-vim.api.nvim_set_keymap("n", "<leader>tz", ":ZenMode<cr>", {silent = true, noremap = true})
-
